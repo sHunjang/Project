67760a7310d853439c29ad960b00381f87edb3a0
sHunjang <https://github.com/sHunjang/Clothing_Coordination.git>
2024-02-07 23:03:07 +0900

    feat : yolo model train epochs 30

--- yolov8.py
+++ yolov8.py
@@ -3,7 +3,10 @@ from ultralytics import YOLO
 dataset_source = '/Users/seunghunjang/Desktop/YOLO_Detect/data.yaml'
 
 # Model Load
-model = YOLO('yolov8m.pt')
+#model = YOLO('yolov8m.pt')
+
+# Pretrained model
+model = YOLO('/opt/homebrew/runs/detect/train5/weights/best.pt')
 
 # Model Train
 model.train(data=dataset_source, epochs=10, imgsz=640)
